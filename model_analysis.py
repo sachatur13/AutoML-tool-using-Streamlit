0b471ff69ead541403010e6168926cd5974e6bc4
Saket Chaturvedi <25706080+sachatur13@users.noreply.github.com>
2021-05-26 10:29:20 +0530

    Added visual changes

--- model_analysis.py
+++ model_analysis.py
@@ -7,6 +7,10 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
 
+st.set_page_config(layout='wide')
+
+st.header('Auto model analysis app')
+st.write('This app helps to identify the best performing model for a preprocessed dataset by leveraging [Autogluon](https://auto.gluon.ai/stable/index.html) library.')
 def func_train_test_split(dataset,target_variable,size_train,size_test):
 
     X = dataset.drop(target_variable,axis = 1)
@@ -23,9 +27,6 @@ def func_classification(train_data,test_data,label):
     predictor = TabularPredictor(label = label,eval_metric = 'f1').fit(train_data)
     
     #test_data = test_data.drop(label,axis = 1)
-
-    st.write('Problem type: ',predictor.problem_type)
-    st.write('Features identified: ',predictor.feature_metadata)
 
     model_leaderboard = predictor.leaderboard(test_data,silent = True)  
     
@@ -45,23 +46,31 @@ dataset = st.sidebar.file_uploader("",)
 
 if dataset is not None:
 
+#### Read dataset from upload
     dataset_source = pd.read_csv(dataset)
+
+#### Change column names to lower case
     dataset_source.columns = dataset_source.columns.str.lower()
+
+#### Identify integer, object and date type variables
     dataset_integer_variables = dataset_source.select_dtypes(include = ['int','int32','float','float32']).columns
     dataset_object_variables = dataset_source.select_dtypes(include = ['O']).columns
     datase_date_variables = dataset_source.select_dtypes(include = ['datetime']).columns
     
     st.header('Select operation')
+
+#### Dataset details section    
     if dataset is not None and st.checkbox('Show dataset details'):
+        
         st.header('Dataset details: ')
         st.write('Number of records: ',dataset_source.shape[0])
         st.write('Number of columns: ',dataset_source.shape[1])
+
         if st.checkbox('Show sample'):
             record_count = st.slider('Select number of records to show',1,20)
             st.table(dataset_source.head(record_count))
 
-    
-    
+#### Data visualization section
     if st.checkbox('Data visualization'):
         visual_type = st.radio('Visualize data',('Univariate','Bi-variate'))
 
@@ -88,17 +97,16 @@ if dataset is not None:
             plt.xlabel(x)
             plt.ylabel(y)
             st.pyplot(fig)
-    
+
+#### Model training section    
     if st.checkbox('Train models'):
         col1,col2 = st.beta_columns(2)
-
     
         with col1:
             target_variable = st.text_input("Input target variable")
             target_variable = target_variable.lower()
             if st.checkbox('Convert target to object type?'):
-                dataset_source[target_variable] = dataset_source[target_variable].astype('O')
-            
+                dataset_source[target_variable] = dataset_source[target_variable].astype('O')          
 
         with col2:
             size_train = np.round(st.number_input('Training size'),2)
@@ -107,30 +115,36 @@ if dataset is not None:
             st.write('Test set size: ',size_test)
 
         if size_train>0:
-            high_frequency = drop_variables(dataset_source)
 
+#### Drop high frequency variables
+            high_frequency = drop_variables(dataset_source)
             dataset_source = dataset_source.drop(high_frequency,axis = 1)
 
+#### Train and testing set creation
             test_data = dataset_source.sample(frac = size_test)
             train_data = dataset_source.sample(frac = size_train)
             
             if st.checkbox('Start model analysis'):
                 predictor,model_leaderboard = func_classification(train_data,test_data,target_variable)
-                st.table(model_leaderboard[['model','score_test','score_val']])
+
+                st.write('Problem type: ',predictor.problem_type)
+                st.write('Features identified: ',predictor.feature_metadata)
                 
-                fig,ax = plt.subplots()
-                ax.barh(model_leaderboard['model'],model_leaderboard['score_val'])
-                plt.xlabel('Evaluated Model')
-                plt.ylabel('Validation score')
-                st.pyplot(fig)
+                with st.beta_expander('View results as table'):
+                    st.table(model_leaderboard[['model','score_test','score_val']])
+
+#### Plot model output
+                with st.beta_expander('View plots'): 
+                    fig,ax = plt.subplots()
+                    ax.barh(model_leaderboard['model'],model_leaderboard['score_val'])
+                    plt.xlabel('Evaluated Model')
+                    plt.ylabel('Validation score')
+                    st.pyplot(fig)
                 
                 trained_models = model_leaderboard['model']
-            
-            if st.checkbox('Load model'):
+
                 model_name = st.sidebar.selectbox('Trained models',trained_models)
                 
-                
-                st.write(pred)
 
 else:
     st.write('Dataset not found. Please upload a dataset first in CSV format')
